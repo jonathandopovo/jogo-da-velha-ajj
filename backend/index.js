de1977af6c09d7b7644b4e45c2308b91e2eb1312
0000723669 <0000723669@senaimgaluno.com.br>
2023-12-06 20:08:59 -0300

    Initializing backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/data", (req, res) => {});
+
+app.post("/app/update", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em: http://localhost:${PORT}`);
+});
