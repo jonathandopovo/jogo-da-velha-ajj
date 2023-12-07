613ee3ec45f59030dd2454778b6661e92942b4f7
0000723669 <0000723669@senaimgaluno.com.br>
2023-12-06 21:33:09 -0300

    Conenncting to database

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("./db/db.config")
 
 app.use(cors());
 app.use(bodyParser.json());
